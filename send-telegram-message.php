ee90b34c07dde456539ebac733e071fa0bf0582e
Trisso90 <157223833+Trisso90@users.noreply.github.com>
2024-01-21 01:13:04 +0700

    Update send-telegram-message.php

--- send-telegram-message.php
+++ send-telegram-message.php
@@ -1,30 +1,36 @@
 <?php
-//
-$chatid = $_POST['chatid_telegram'];
-$pesan = $_POST['pesan_telegram'];
 
-// Kirim pesan ke bot Telegram (gunakan token bot Anda)
-$botToken = '6946290282:AAHbPsb9hD-DbepVOIkge81AvWjY90YbbU0'; // Gantilah 'your_bot_token' dengan token bot Anda
-$telegramURL = "https://api.telegram.org/bot$botToken/sendMessage";
-$data = array('chat_id' => $6824078885, 'text' => $pesan);
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Ambil data dari formulir
+    $pesan = $_POST['pesan'];
 
-$options = array(
-    'http' => array(
-        'header' => "Content-type: application/x-www-form-urlencoded\r\n",
-        'method' => 'POST',
-        'content' => http_build_query($data),
-    ),
-);
+    // Kirim pesan ke bot Telegram (gantilah 'your_bot_token' dengan token bot Anda)
+    $botToken = '6946290282:AAHbPsb9hD-DbepVOIkge81AvWjY90YbbU0';
+    $telegramURL = "https://api.telegram.org/bot$botToken/sendMessage";
+    $data = array('chat_id' => $chatid, 'text' => $pesan);
 
-$context = stream_context_create($options);
-$result = file_get_contents($telegramURL, false, $context);
+    $options = array(
+        'http' => array(
+            'header' => "Content-type: application/x-www-form-urlencoded\r\n",
+            'method' => 'POST',
+            'content' => http_build_query($data),
+        ),
+    );
 
-// Cek jika pesan terkirim berhasil atau tidak
-if ($result === false) {
-    echo "Pesan gagal dikirim ke Telegram";
+    $context = stream_context_create($options);
+    $result = file_get_contents($telegramURL, false, $context);
+
+    // Cek jika pesan terkirim berhasil atau tidak
+    if ($result === false) {
+        echo "Pesan gagal dikirim ke Telegram";
+    } else {
+        echo "Pesan berhasil dikirim ke Telegram";
+    }
+
+    // Di sini Anda bisa menambahkan logika atau operasi lain yang diperlukan
 } else {
-    echo "Pesan berhasil dikirim ke Telegram";
+    echo "Metode request tidak valid.";
 }
 
-// Di sini Anda bisa menambahkan logika atau operasi lain yang diperlukan
 ?>
+
